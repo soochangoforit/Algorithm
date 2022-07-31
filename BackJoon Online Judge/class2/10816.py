bafe646ad58cddda8aca88438303a39558c3cddd
soochan <tncksdl05@gmail.com>
2022-07-31 15:59:48 +0900

    using hashmap algorithm for 10816

--- BackJoon Online Judge/class2/10816.py
+++ BackJoon Online Judge/class2/10816.py
@@ -1,34 +1,25 @@
 # https://www.acmicpc.net/problem/10816
 # https://chancoding.tistory.com/45
 
-# 앞전 코드의 문제점을 해결하고자, 새로운 코드 작성
-# M에 속해있는 갯수만 구하고자 한다. 이미 한번 카운트된 숫자는 다음 숫자의 카운트 시에 제외하고자 한다.
+from sys import stdin
 
-import sys
+_ = stdin.readline()
+N = map(int,stdin.readline().split())
+_ = stdin.readline()
+M = map(int,stdin.readline().split())
 
+hashmap = {} # HashMap에는 N리스트가 가지고 있는 원소가 Key가 되고 , 개수가 value가 된다.
 
-_ = sys.stdin.readline()
-N = sorted(map(int , sys.stdin.readline().split()))
-_ = sys.stdin.readline()
-M = list(map(int , sys.stdin.readline().split())) # M에서 제공된 숫자에 맡게끔 출력을 해야하기 때문에 Sorted는 m_dic에 값을 넣을때 사용한다.
+# HashMap의 사용은 전적으로 N을 모두 순회하면서 Count value를 남기고자 한다.
+for n in N:
+    if n in hashmap: # HaspMap에소 hasing에 의해 이미 원소를 가지고 있으면 해당 value 값을 1 증가시킨다.
+        hashmap[n] += 1 
+    else:            # N을 순회하면서 HashMap에 값이 없으면 처음으로 count하는 의미이므로 value에 1를 넣어준다.
+        hashmap[n] = 1
 
-index = 0 # N 리스트의 index
-m_dic = {} # M리스트에 있는 숫자의 개수를 dic 형태로 저장하고자 한다.
+# 출력할때는 M을 순회하면서 N에서 찾는게 아니라 HashMap에서 해당 Key가 있는지 확인한다.
+print(' '.join(str(hashmap[m]) if m in hashmap else '0' for m in M)) 
 
-for m in sorted(M): 
-    cnt = 0 # 찾고자 하는 숫자가 발견된 수
-    if m not in m_dic: # m 숫자의 개수를 N 리스트에서 찾을려고 할때 한번에 모두 찾기 때문에, m에 대해서는 같은 숫자에 대해 반복적으로 찾지 않는다.
-        while index < len(N):
-            if m == N[index]: # 찾고자 하는 수가 N 리스트에 있을 때
-                cnt += 1
-                index += 1 # 있으면 다음 N리스트로 접근한다.
-            elif m > N[index]: # 찾고자 하는 값이 N 리스트보다 클때, 다음 index로 넘어간다
-                index += 1
-            else: # m < N[index] -> else라는 의미는 우리가 찾고자 했던 m이 N 리스트에 존재하지 않는다는 것이다.
-                break # break를 통해서 찾고자 했던 숫자 m에 대해서는 cnt가 0으로 가진다.
-            
-            #while문 종료, 찾고자 했던 m에 대한 cnt 값을 구한다.
-        # if문 종료, m_dic에 찾고자 했던 숫자의 cnt값을 key 값에 맞춰서 넣어준다.    
-        m_dic[m] = cnt
-
-print(' '.join(str(m_dic[m]) for m in M))
+# 이번 풀이의 가장 큰 특징은 비록 M이 아닌 N이 가지고 있는 모든 숫자에 대해서 count를 하지만, hash 자료구조의 특징을 활용해서 
+# key로 작용하는 n에 대해서 , hashmap에 n이 있으면 해당 dic에서 value를 +1 해주고 없으면 1를 할당해준다.
+# 1를 할당해주는 이유는 우리는 지금 M에 대해서가 아닌 , N에 대해서 숫자의 개수를 파악하기 위해서 이다.
