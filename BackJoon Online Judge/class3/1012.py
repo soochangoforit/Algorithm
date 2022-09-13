52722477ef0578cac3e8394a42c7eaa264249918
soochan <tncksdl05@gmail.com>
2022-09-14 01:43:44 +0900

    add coment on solved problem

--- BackJoon Online Judge/class3/1012.py
+++ BackJoon Online Judge/class3/1012.py
@@ -8,31 +8,44 @@ t = int(input())
 def bfs(graph, a, b):
     queue = deque()
     queue.append((a,b))
+    # 지나온 위치 0으로 바꾸기
     graph[a][b] = 0
 
     while queue:
         x, y = queue.popleft()
+        # 4방향으로 1이면 0으로 바꾸기
         for i in range(4):
             nx = x+dx[i]
             ny = y+dy[i]
-            if nx < 0 or nx >=n or ny < 0 or ny >= m:
+            if nx < 0 or nx >=M or ny < 0 or ny >=N :
                 continue
             if graph[nx][ny] == 1:
                 graph[nx][ny] = 0
+                # bfs 방법이므로 4방향이 다 끝나고 접근하기 위해 queue에 넣는다.
                 queue.append((nx, ny))
     return
 
+
+
 for i in range(t):
+    # 배추흰지렁이의 수
     cnt = 0
-    n, m, k = map(int,input().split())
-    graph = [[0]*m for _ in range(n)]
+    M, N, K = map(int,input().split())
 
-    for j in range(k):
+    # 가로 M줄, 세로 N줄
+    # graph = [N][M]
+    graph = [[0]*M for _ in range(N)]
+
+    # 배추 위치 표시
+    for j in range(K):
+        # x: 열, y: 행
         x, y = map(int, input().split())
-        graph[x][y] = 1
+        graph[y][x] = 1
 
-    for a in range(n):
-        for b in range(m):
+    # 배추흰지렁이 수 구하기
+    # M : 열 , N : 행
+    for a in range(N):
+        for b in range(M):
             if graph[a][b] == 1:
                 bfs(graph, a, b)
                 cnt += 1
