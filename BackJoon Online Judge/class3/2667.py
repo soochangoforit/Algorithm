80a161e977dc625a5ccf32abb5461ac4d77a63f2
soochan <tncksdl05@gmail.com>
2022-10-04 22:26:57 +0900

    solved with bfs

--- BackJoon Online Judge/class3/2667.py
+++ BackJoon Online Judge/class3/2667.py
@@ -1,48 +1,57 @@
 # https://www.acmicpc.net/problem/2667
 
+from collections import deque
 import sys
-
 
 N = int(sys.stdin.readline().rstrip())
 
 miro = []
 
 for i in range(N):
-    miro.append(list(map(int,sys.stdin.readline().rstrip())))
+    miro.append(list(map(int, sys.stdin.readline().rstrip())))
 
 # 아래, 오른쪽, 위, 왼쪽
 dy = (1,0,-1,0)
 dx = (0,1,0,-1)
 
-# 단지수를 저장한 변수
 count = 0
 
-# 단지별 집의 수를 저장할 리스트
 result = []
 
-def dfs(y,x):
-    global count
-    # 한번 방문했기 때문에 집의 수 +1 해준다.
-    count += 1
-    # 방문한 곳은 0으로 바꿔준다. 추가적인 방문처리하기 위한 메모리를 절약하기 위함
+def bfs(y,x):
+    global count 
+    Q = deque()
+    # 방문했다는 의미로 0 할당
     miro[y][x] = 0
-    # 4방향 탐색
-    for i in range(4):
-        next_y = y + dy[i]
-        next_x = x + dx[i]
-        # 범위를 벗어나지 않고, 집이 1로 존재해야 한다, 이미 방문한 곳에 대해서는 0으로 바꿨기 때문에 0이 아닌 곳을 방문한다.
-        if 0<= next_y < N and 0<= next_x <N and miro[next_y][next_x] == 1:
-            dfs(next_y,next_x)
+    Q.append((y,x))
+
+    while Q:
+        y,x = Q.popleft()
+
+        for i in range(4):
+            next_y = y + dy[i]
+            next_x = x + dx[i]
+
+            if 0 <= next_y < N and 0 <= next_x < N and miro[next_y][next_x] == 1:
+                # deque에 넣을 원소에 대해서 미리 방문 처리를 해준다.
+                # Q에 같은 노드가 중복으로 들어가는것을 방지하기 위해서 곧 방문할 노드에 대해서 미리 방문처리를 해준다. - 중요
+                miro[next_y][next_x] = 0
+                Q.append((next_y, next_x))
+                count += 1
+
+
 
 for i in range(N):
     for j in range(N):
+        # 1일 경우에만 BFS를 실행한다.
         if miro[i][j] == 1:
-            dfs(i,j)
+            count += 1
+            bfs(i,j)
             result.append(count)
-            # 단지별 집의 수 초기화
+            # 한번의 BFS가 끝나면 count를 초기화 해준다.
+            # 다음으로 나오는 단지에 대한 집의 수를 구하기 위해서
             count = 0
 
 print(len(result))
 for i in sorted(result):
     print(i)
-
