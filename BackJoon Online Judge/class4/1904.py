98ae6a9b420b9e581cb525c0cb2bec1d968045e4
soochan <tncksdl05@gmail.com>
2022-12-17 23:44:36 +0900

    learning dp with python

---
+++ BackJoon Online Judge/class4/1904.py
@@ -0,0 +1,17 @@
+# https://www.acmicpc.net/problem/1904
+
+import sys  
+
+n = int(sys.stdin.readline())
+
+dp = [0] * (n+1)
+
+dp[1] = 1
+
+if n > 1:
+    dp[2] = 2
+    
+    for i in range(3, n+1):
+        dp[i] = (dp[i-1] + dp[i-2]) % 15746
+
+print(dp[n])
