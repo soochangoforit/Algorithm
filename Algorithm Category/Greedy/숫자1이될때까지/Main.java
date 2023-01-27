cd3fd01ee1513f1c8b484eba2bf750fdc16530f2
soochan <tncksdl05@gmail.com>
2023-01-28 00:36:23 +0900

    1이될때까지.java

---
+++ Algorithm Category/Greedy/숫자1이될때까지/Main.java
@@ -0,0 +1,31 @@
+package 숫자1이될때까지;
+
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int result = 0;
+
+        while (true) {
+            int target = (n / k) * k;
+            result += ( n - target);
+
+            n = target;
+            if (n <k) break;
+
+            result += 1;
+            n /= k;
+        }
+
+        result += (n-1);
+        System.out.println(result);
+
+
+    }
+}
+
