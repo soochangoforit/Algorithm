381af179edc1029262e4f7f6998332ab7259f9e5
soochan <tncksdl05@gmail.com>
2023-01-29 19:39:48 +0900

    곱하기혹은더하기.java

---
+++ Algorithm Category/Greedy/곱하기혹은더하기/Main.java
@@ -0,0 +1,25 @@
+package 곱하기혹은더하기;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        String str = sc.next();
+
+        int result = str.charAt(0) - '0';
+        for (int i = 1; i < str.length(); i++) {
+            
+            int num = str.charAt(i) - '0';
+            if (num <= 1 || result <= 1 ){
+                result += num;
+            }
+            else {
+                result *= num;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
