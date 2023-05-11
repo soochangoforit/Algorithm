c97e175da19014d0ac0598c5d52a6f1e67f5afb7
soochan <tncksdl05@gmail.com>
2023-05-11 09:20:19 +0900

    DoublePermutation.java

---
+++ Algorithm Category/DFS,BFS/중복 순열/DoublePermutation.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class DoublePermutation {
+    static int[] pm;
+    static int n, m;
+    public void DFS(int L) {
+        if(L== m) {
+            // ≈ª√‚ ¡∂∞«
+            for(int x : pm) System.out.print(x + " ");
+            System.out.println();
+        }
+        else {
+            for(int i = 1; i <= n; i++) {
+                pm[L] = i;
+                DFS(L+1);
+            }
+        }
+
+
+    }
+
+    public static void main(String[] args){
+        DoublePermutation T = new DoublePermutation();
+        Scanner kb = new Scanner(System.in);
+        n = kb.nextInt();
+        m = kb.nextInt();
+        pm = new int[m];
+        T.DFS(0);
+    }
+}
